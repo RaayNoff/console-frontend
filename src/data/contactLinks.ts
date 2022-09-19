a37c7c9160d586d48c8d0d307c544fa8cef1f578
RaayNoff <kraxtv471365@gmail.com>
2022-09-19 20:46:17 +0700

    Added contact links

---
+++ src/data/contactLinks.ts
@@ -0,0 +1,6 @@
+export enum ContactLinks {
+  TG = "https://t.me/raaaynoff",
+  DISCORD = "https://discordapp.com/users/210826819333521414/",
+  VK = "https://vk.com/dmitryoks",
+  GITHUB = "https://github.com/RaayNoff",
+}
