a9d250dc61db7889f7f1c03d407de6faad990cf6
RaayNoff <kraxtv471365@gmail.com>
2022-09-20 16:20:07 +0700

    Added default structure of homepage

--- src/components/pages/home/HomePage.component.tsx
+++ src/components/pages/home/HomePage.component.tsx
@@ -1,8 +1,28 @@
 import { FC } from "react";
-import MainLayoutComponent from "../../layouts/main";
+import ContactsComponent from "../../buisness/contacts/Contacts.component";
+import MainLayout from "../../layouts/main/mainLayout";
+import s from "./homepage.module.scss";
 
 const HomePageComponent: FC = () => {
-  return <MainLayoutComponent children={"Test string"} />;
+  return (
+    <MainLayout>
+      <main className={s.home}>
+        <header className={s.home__title} data-language="ru">
+          Hi! My name is Dmitry, also known as Raaynoff. I am a junior frontend
+          developer. I am very interested in the web and everything related to
+          it. Here you can find comprehensive information about how to contact
+          me and a little about me.
+        </header>
+        <section className={s.home__content}>
+          <article className={s.home__about}>About me</article>
+          <aside className={s.home__aside}></aside>
+        </section>
+        <footer className={s.home__contact}>
+          <ContactsComponent></ContactsComponent>
+        </footer>
+      </main>
+    </MainLayout>
+  );
 };
 
 export default HomePageComponent;
