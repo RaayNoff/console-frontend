b0e893d89e0d6b64fd5c027054c7902b1b6b9bf7
RaayNoff <kraxtv471365@gmail.com>
2022-09-20 20:23:37 +0700

    Changed structure and style

--- src/components/pages/home/HomePage.component.tsx
+++ src/components/pages/home/HomePage.component.tsx
@@ -1,5 +1,7 @@
 import { FC } from "react";
 import ContactsComponent from "../../buisness/contacts/Contacts.component";
+import NavigationComponent from "../../buisness/navigation/Navigation.component";
+import WelcomeComponent from "../../buisness/welcome/Welcome.component";
 import MainLayout from "../../layouts/main/mainLayout";
 import s from "./homepage.module.scss";
 
@@ -7,15 +9,16 @@ const HomePageComponent: FC = () => {
   return (
     <MainLayout>
       <main className={s.home}>
-        <header className={s.home__title} data-language="ru">
-          Hi! My name is Dmitry, also known as Raaynoff. I am a junior frontend
-          developer. I am very interested in the web and everything related to
-          it. Here you can find comprehensive information about how to contact
-          me and a little about me.
-        </header>
-        <section className={s.home__content}>
-          <article className={s.home__about}>About me</article>
-          <aside className={s.home__aside}></aside>
+        <h1 className={s.home__title} data-language="en">
+          Dmitry Tarasenko
+        </h1>
+        <section className={s.home__container}>
+          <section className={s.home__content}>
+            <div className={s.home__welcome}>
+              <WelcomeComponent />
+            </div>
+            <NavigationComponent />
+          </section>
         </section>
         <footer className={s.home__contact}>
           <ContactsComponent></ContactsComponent>
