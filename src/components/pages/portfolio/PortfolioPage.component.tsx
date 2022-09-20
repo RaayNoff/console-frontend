4e1e80e3c87820ef04513bc354da7bc2dd268e65
RaayNoff <kraxtv471365@gmail.com>
2022-09-20 20:24:58 +0700

    Added base for other pages

---
+++ src/components/pages/portfolio/PortfolioPage.component.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from "react";
+import MainLayout from "../../layouts/main/mainLayout";
+import s from "./portfolioPage.module.scss";
+
+const PortfolioPageComponent: FC = () => {
+  return <MainLayout>Portfolio</MainLayout>;
+};
+
+export default PortfolioPageComponent;
---
+++ src/components/pages/skills/SkillsPage.component.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from "react";
+import MainLayout from "../../layouts/main/mainLayout";
+import s from "./skillspage.module.scss";
+
+const SkillsPageComponent: FC = () => {
+  return <MainLayout>Skills!</MainLayout>;
+};
+
+export default SkillsPageComponent;
