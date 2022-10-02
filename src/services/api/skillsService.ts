35457a39b0eb7dcb614287d01861285be354a6cd
RaayNoff <kraxtv471365@gmail.com>
2022-10-02 18:12:42 +0700

    Added rest of mutations

--- src/services/api/skillsService.ts
+++ src/services/api/skillsService.ts
@@ -2,6 +2,10 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import backendEndpoints from "../../data/classes/backendEndpoints";
 import { ISkill } from "../../models/skill";
 
+interface IDelete {
+  id: number;
+}
+
 export const skillsService = createApi({
   reducerPath: "skillsService",
   refetchOnReconnect: true,
@@ -17,6 +21,32 @@ export const skillsService = createApi({
         method: "GET",
       }),
       providesTags: ["Skills"],
+    }),
+    createSkill: build.mutation<ISkill, FormData>({
+      query: (formData) => ({
+        url: backendEndpoints.skills,
+        method: "POST",
+        body: formData,
+      }),
+      invalidatesTags: ["Skills"],
+    }),
+    deleteSkill: build.mutation<ISkill, IDelete>({
+      query: ({ id }) => ({
+        url: backendEndpoints.skills + `/${id}`,
+        method: "DELETE",
+        body: {
+          key: localStorage.getItem("key"),
+        },
+      }),
+      invalidatesTags: ["Skills"],
+    }),
+    updateSkill: build.mutation<ISkill, FormData>({
+      query: (formData) => ({
+        url: backendEndpoints.skills,
+        method: "PUT",
+        body: formData,
+      }),
+      invalidatesTags: ["Skills"],
     }),
   }),
 });
