07bfcadd66607fd4164dce328e6cefc6f55a82c6
RaayNoff <kraxtv471365@gmail.com>
2022-10-02 18:12:33 +0700

    Skill component now has prop id

--- src/components/pages/skills/SkillsPage.component.tsx
+++ src/components/pages/skills/SkillsPage.component.tsx
@@ -4,7 +4,7 @@ import { useTypedSelector } from "../../../hooks/useTypedSelector";
 import { skillsService } from "../../../services/api/skillsService";
 import SkillCreatorComponent from "../../buisness/skillCreator/SkillCreator.component";
 import MainLayout from "../../layouts/main/mainLayout";
-import SkillComponent from "../../ui/skill/Skill.component";
+import SkillComponent from "../../buisness/skill/Skill.component";
 import s from "./skillspage.module.scss";
 
 const SkillsPageComponent: FC = () => {
@@ -23,6 +23,7 @@ const SkillsPageComponent: FC = () => {
           <section className={s.skills_list}>
             {skills?.map((skill) => (
               <SkillComponent
+                id={skill.id}
                 key={skill.id}
                 image={skill.image}
                 name={skill.name}
